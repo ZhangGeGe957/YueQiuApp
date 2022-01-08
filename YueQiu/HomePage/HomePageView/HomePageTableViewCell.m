8846aa704ba80301222b23e354a886dc4dd41557
haoqianbiao <464408508@qq.com>
2022-01-08 21:40:00 +0800

    1.8 21:39

---
+++ YueQiu/HomePage/HomePageView/HomePageTableViewCell.m
@@ -0,0 +1,18 @@
+//
+//  HomePageTableViewCell.m
+//  YueQiu
+//
+//  Created by haoqianbiao on 2022/1/8.
+//
+
+#import "HomePageTableViewCell.h"
+
+@implementation HomePageTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    return self;
+}
+
+
+@end
--- YueQiu/HomePage/HomePageView/HomePageUIView.m
+++ YueQiu/HomePage/HomePageView/HomePageUIView.m
@@ -7,6 +7,7 @@
 
 #import "HomePageUIView.h"
 #import "HomePageMModel.h"
+#import "HomePageTableViewCell.h"
 #import "Masonry.h"
 
 #define W [UIScreen mainScreen].bounds.size.width
@@ -17,20 +18,33 @@
     self = [super initWithFrame:frame];
     
     self.tableView = [[UITableView alloc] init];
-    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self).mas_offset(H / 20);
-            make.left.equalTo(self).mas_offset(W*0.3);
-            make.width.mas_equalTo(0.5*W);
-            make.height.mas_equalTo(0.01 * H);
-    }];
     [self addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self).offset(H / 18);
+            make.left.equalTo(self).offset(W * 0.25);
+            make.width.mas_offset(0.5 * W);
+            make.height.mas_offset(0.05 * H);
+    }];
+    
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    self.searchController = [[UISegmentedControl alloc] init]
+    
+    [self.tableView registerClass:[HomePageTableViewCell class] forCellReuseIdentifier:@"0"];
+    
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    
     
     return self;
 }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.dataArray count];
+}
 
-
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    HomePageTableViewCell* cell = [self.tableView dequeueReusableCellWithIdentifier:@"0"];
+    return cell;
+}
 
 @end
