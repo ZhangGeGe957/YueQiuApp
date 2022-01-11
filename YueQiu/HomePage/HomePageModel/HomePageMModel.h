009c76b49cb526c932d8191e3eceb9d42a30a64d
haoqianbiao <464408508@qq.com>
2022-01-11 19:04:45 +0800

    1.10  19:05

--- YueQiu/HomePage/HomePageModel/HomePageMModel.h
+++ YueQiu/HomePage/HomePageModel/HomePageMModel.h
@@ -10,7 +10,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HomePageMModel : NSObject
-
+@property (nonatomic, strong) NSMutableArray* dataArray;
+@property (nonatomic, strong) NSMutableArray* searchArray;
 @end
 
 NS_ASSUME_NONNULL_END
