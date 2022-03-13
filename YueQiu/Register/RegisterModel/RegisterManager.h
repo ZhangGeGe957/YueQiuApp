e75a5b0cc45ecf727c5f012e6849e50f9232ba93
ZhangGeGe957 <1819535257@qq.com>
2022-03-13 13:24:03 +0800

    新增jsonmodel库 3.13 13:23

---
+++ YueQiu/Register/RegisterModel/RegisterManager.h
@@ -0,0 +1,16 @@
+//
+//  RegisterManager.h
+//  YueQiu
+//
+//  Created by 张佳乔 on 2022/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RegisterManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YueQiu/Register/RegisterModel/RegisterManager.m
@@ -0,0 +1,12 @@
+//
+//  RegisterManager.m
+//  YueQiu
+//
+//  Created by 张佳乔 on 2022/3/13.
+//
+
+#import "RegisterManager.h"
+
+@implementation RegisterManager
+
+@end
